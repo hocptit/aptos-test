073499449085346266cbfe0d99defb0ffa6bfb42
Patrick Nguyen <nguyenthaihoc.dev@gmail.com>
2024-04-26 10:05:01 +0700

    ag

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
 import { Account, Ed25519PublicKey } from '@aptos-labs/ts-sdk';
 const ed25519PublicKey = new Ed25519PublicKey("publicKey");
 const accountAddress = Account.authKey({ publicKey: ed25519PublicKey }).derivedAddress().toString();
-//
 console.log(accountAddress)
